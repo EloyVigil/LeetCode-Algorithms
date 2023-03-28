acecc94d0d50d6cc1b968ee584529728ae79e717
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2023-03-27 23:07:16 -0700

    Solution in JavaScript

--- RemoveElement.js
+++ RemoveElement.js
@@ -5,6 +5,45 @@ var removeElement = function (nums, val) {
     return nums.length;
 };
 
+
+const assert = require('assert');
+
+function testRemoveElement() {
+    // Test Case 1
+    const arr1 = [3, 2, 2, 3];
+    const val1 = 3;
+    const expectedOutput1 = 2;
+    const actualOutput1 = removeElement(arr1, val1);
+    assert.strictEqual(actualOutput1, expectedOutput1);
+    console.log('Test Case 1 Passed');
+    
+    // Test Case 2
+    const arr2 = [0, 1, 2, 2, 3, 0, 4, 2];
+    const val2 = 2;
+    const expectedOutput2 = 5;
+    const actualOutput2 = removeElement(arr2, val2);
+    assert.strictEqual(actualOutput2, expectedOutput2);
+    console.log('Test Case 2 Passed');
+    
+    // Test Case 3
+    const arr3 = [];
+    const val3 = 0;
+    const expectedOutput3 = 0;
+    const actualOutput3 = removeElement(arr3, val3);
+    assert.strictEqual(actualOutput3, expectedOutput3);
+    console.log('Test Case 3 Passed');
+    
+    // Test Case 4
+    const arr4 = [1, 2, 3, 4];
+    const val4 = 5;
+    const expectedOutput4 = 4;
+    const actualOutput4 = removeElement(arr4, val4);
+    assert.strictEqual(actualOutput4, expectedOutput4);
+    console.log('Test Case 4 Passed');
+}
+
+testRemoveElement();
+
 // // Import assert module
 // const assert = require('assert');
 
@@ -31,42 +70,3 @@ var removeElement = function (nums, val) {
 //         assert.strictEqual(removeElement([1, 2, 3, 4], 5), 4);
 //     });
 // });
-
-
-const assert = require('assert');
-
-function testRemoveElement() {
-    // Test Case 1
-    const arr1 = [3, 2, 2, 3];
-    const val1 = 3;
-    const expectedOutput1 = 2;
-    const actualOutput1 = removeElement(arr1, val1);
-    assert.strictEqual(actualOutput1, expectedOutput1);
-    console.log('Test Case 1 Passed');
-
-    // Test Case 2
-    const arr2 = [0, 1, 2, 2, 3, 0, 4, 2];
-    const val2 = 2;
-    const expectedOutput2 = 5;
-    const actualOutput2 = removeElement(arr2, val2);
-    assert.strictEqual(actualOutput2, expectedOutput2);
-    console.log('Test Case 2 Passed');
-
-    // Test Case 3
-    const arr3 = [];
-    const val3 = 0;
-    const expectedOutput3 = 0;
-    const actualOutput3 = removeElement(arr3, val3);
-    assert.strictEqual(actualOutput3, expectedOutput3);
-    console.log('Test Case 3 Passed');
-
-    // Test Case 4
-    const arr4 = [1, 2, 3, 4];
-    const val4 = 5;
-    const expectedOutput4 = 4;
-    const actualOutput4 = removeElement(arr4, val4);
-    assert.strictEqual(actualOutput4, expectedOutput4);
-    console.log('Test Case 4 Passed');
-}
-
-testRemoveElement();
