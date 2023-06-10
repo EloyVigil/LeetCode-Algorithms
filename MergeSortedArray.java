d7dce0f958eacbbcd980b7694173fc7088e68498
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2023-06-09 19:21:41 -0700

    Solution in Java

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -43,6 +43,7 @@ public class MergeSortedArray {
         runTest(new int[]{1}, 1, new int[]{}, 0, new int[]{1});
         runTest(new int[]{0}, 0, new int[]{1}, 1, new int[]{1});
         runTest(new int[]{4, 5, 6, 0, 0, 0}, 3, new int[]{1, 2, 3}, 3, new int[]{1, 2, 3, 4, 5, 6});
+        runTest(new int[]{4, 5, 6, 0, 0, 0}, 3, new int[]{1, 2, 3}, 3, new int[]{1, 2, 4, 5, 6, 6});
     }
 }
 
