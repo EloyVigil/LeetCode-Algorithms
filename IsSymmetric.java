821038cf8f9add9667f0db4aef736ff36b3c9a1d
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2023-06-16 23:02:25 -0700

    Solution in Java

---
+++ IsSymmetric.java
@@ -0,0 +1,34 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class IsSymmetric {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        return isSame(root.left, root.right);
+    }
+
+    private boolean isSame(TreeNode leftroot, TreeNode rightroot) {
+        if ((leftroot == null && rightroot != null) || (leftroot != null && rightroot == null)
+                || (leftroot != null && rightroot != null && leftroot.val != rightroot.val))
+            return false;
+        if (leftroot != null && rightroot != null)
+            return isSame(leftroot.left, rightroot.right) && isSame(leftroot.right, rightroot.left);
+        return true;
+    }
+}
